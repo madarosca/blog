e70a191256059bc57be6db21bef79f88f41f851d
Mada Rosca <mada.rosca@yahoo.com>
2016-10-17 22:11:19 +0300

    initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,21 @@
+<html>
+    @extends('layouts.app')
+    @section('title', 'Page Title')
+    
+    @section('content')
+    <body>
+        <div class="row">
+            <div class="content col-md-8">
+                <h1 class="title text-info text-center">Welcome to Mada's blog</h1>
+            </div>
+            <div class="button col-md-4">
+                <button type="button" class="btn btn-primary">Just a button</button>
+            </div>
+            <hr>
+            </div>
+        </div>
+    </body>
+    @endsection
+</html>
+
+
